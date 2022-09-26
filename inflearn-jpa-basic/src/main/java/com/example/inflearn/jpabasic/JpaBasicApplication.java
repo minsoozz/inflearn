2e0fcb7cec32988dace496c46d9d6a21e133e6cd
minsoozz <minsoo1604@naver.com>
2022-09-27 01:29:45 +0900

    chore: 프로젝트 설정 추가

---
+++ inflearn-jpa-basic/src/main/java/com/example/inflearn/jpabasic/JpaBasicApplication.java
@@ -0,0 +1,12 @@
+package com.example.inflearn.jpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaBasicApplication.class);
+  }
+}
