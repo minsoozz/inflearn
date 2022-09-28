48a50297ad1771606e52ce6f75c383b14a5d65bc
minsoozz <minsoo1604@naver.com>
2022-09-29 00:36:33 +0900

    feat: Hello JPA - 애플리케이션 개발

--- inflearn-jpa-basic/src/main/java/com/example/inflearn/jpabasic/JpaBasicApplication.java
+++ inflearn-jpa-basic/src/main/java/com/example/inflearn/jpabasic/JpaBasicApplication.java
@@ -1,12 +1,38 @@
 package com.example.inflearn.jpabasic;
 
-import org.springframework.boot.SpringApplication;
+import com.example.inflearn.jpabasic.entity.Member;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaBasicApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(JpaBasicApplication.class);
-  }
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            List<Member> results = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for (Member result : results) {
+                System.out.println("result.getName() = " + result.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+
+        }
+        emf.close();
+    }
 }
